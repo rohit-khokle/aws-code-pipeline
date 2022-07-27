c518f7eacf909b740ba543ec9dce3e37959fd40d
Rohit Khokle <khokle.r@northeastern.edu>
2022-07-27 16:19:46 -0400

    first commit

---
+++ src/main/java/com/sampleproject/demo/DemoController.java
@@ -0,0 +1,14 @@
+package com.sampleproject.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hi from Project - v1";
+    }
+}
