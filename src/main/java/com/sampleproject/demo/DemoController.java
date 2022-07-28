bc6fab0b32e2abee596e84a22395721f48cd0b92
MATHWORKS\rkhokle <rkhokle@mathworks.com>
2022-07-28 16:29:22 -0400

    Adds Mapping

--- src/main/java/com/sampleproject/demo/DemoController.java
+++ src/main/java/com/sampleproject/demo/DemoController.java
@@ -1,10 +1,10 @@
 package com.sampleproject.demo;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping({"/v1/demo"})
 public class DemoController {
 
     @GetMapping("/hello")
